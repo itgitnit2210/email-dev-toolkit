500d0fdcc2df525ae3ab49364c0c695af6d83e27
itgitnit2210 <thakurprofessional2210@gmail.com>
2026-08-19 11:28:55 +0530

    Register email autocomplete provider

--- src/extension.js
+++ src/extension.js
@@ -6,12 +6,14 @@ const { registerInsertColumnsCommand } = require('./commands/insertColumns');
 const { registerInsertButtonCommand } = require('./commands/insertButton');
 const { registerInsertResponsiveImageCommand } = require('./commands/insertResponsiveImage');
 const { registerValidateCurrentEmailCommand } = require('./commands/validateCurrentEmail');
+const { registerEmailCompletionProvider } = require('./completions/registerEmailCompletions');
 
 function activate(context) {
   const outputChannel = vscode.window.createOutputChannel('Email Dev Toolkit');
 
   context.subscriptions.push(
     outputChannel,
+    registerEmailCompletionProvider(),
     registerInsertTemplateCommand(),
     registerConfigureLayoutCommand(),
     registerInsertColumnsCommand(),
